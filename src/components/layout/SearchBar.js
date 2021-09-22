9b5b8568299d145e6a7e695e3ca6c504a663da6f
Bhupendersinghbisht <bsrocking@gmail.com>
2021-09-22 07:59:45 +0530

    logs done

--- src/components/layout/SearchBar.js
+++ src/components/layout/SearchBar.js
@@ -1,12 +1,18 @@
-import React from 'react'
-
-const SearchBar = () => {
+import React,{useRef} from 'react'
+import {connect} from 'react-redux'
+import PropTypes from 'prop-types';
+import { searchLogs } from '../../actions/logActions';
+const SearchBar = ({searchLogs}) => {
+  const text=useRef('')
+const onChange =() =>{
+  searchLogs(text.current.value)
+}
     return (
         <nav style={{marginBottom:'30px'}} className="red">
         <div className="nav-wrapper">
           <form>
             <div className="input-field">
-              <input id="search" type="search"/>
+              <input id="search" type="search" placeholder='Search Value..'  ref={text} onChange={onChange}/>
               <label className="label-icon" htmlFor="search"><i className="material-icons">search</i></label>
               <i className="material-icons">close</i>
             </div>
@@ -16,4 +22,7 @@ const SearchBar = () => {
     )
 }
 
-export default SearchBar
+SearchBar.propTypes={
+  searchLogs:PropTypes.func.isRequired,
+}
+export default connect(null,{searchLogs})(SearchBar)
--- src/components/logs/EditLogModal.js
+++ src/components/logs/EditLogModal.js
@@ -1,17 +1,36 @@
-import React,{useState} from 'react'
+import React,{useState,useEffect} from 'react'
 import M from 'materialize-css/dist/js/materialize.min.js'
-const EditLogModal = () => {
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
+import { updateLog } from '../../actions/logActions'
+const EditLogModal = ({current,updateLog}) => {
     const [message,setMessage]=useState('')
     const [attention,setAttention]=useState(false);
     const [tech,setTech]=useState('');
 
+    useEffect(() =>{
+        if(current){
+            setMessage(current.message)
+            setAttention(current.attention)
+            setTech(current.tech)
+        }
+    },[current])
     const onSubmit =() =>{
         if(message === '' || tech === ''){
             M.toast({html:'Please enter a message and tech'})
         }
         else{
-        console.log(message,tech,attention)
+        const updLog={
+            id:current.id,
+            message,
+            attention,
+            tech,
+            date: new Date()
+        }
 
+            console.log(message,tech,attention)
+updateLog(updLog)
+M.toast({html :`log udpated by ${tech}`})
         //clear fields
         setMessage('');
         setTech('');
@@ -68,4 +87,11 @@ const modalStyle={
     width:'75%',
     height:'75%'
 }
-export default EditLogModal
+EditLogModal.propTypes={
+current:PropTypes.object,
+updateLog:PropTypes.func.isRequired,
+}
+const mapStateToProps= state => ({
+    current:state.log.current
+})
+export default connect(mapStateToProps,{updateLog})(EditLogModal)
